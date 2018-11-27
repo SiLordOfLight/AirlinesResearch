ebfcf71dba19ef40460cd2ff4d7fa74ef44c7619
SiLordOfLight <jatnerd@gmail.com>
2018-11-27 10:47:51 -0800

    11/27-2

--- webapp.py
+++ webapp.py
@@ -5,11 +5,12 @@ import airline_fx as afx
 
 app = Flask(__name__)
 
-navItems = [{"page":"/", "title":"Home", "id":"home"}, {"page":"research", "title":"Research", "id":"research"}, {"page":"analysis", "title":"Analysis", "id":"analysis"}]
+navItems = [{"page":"/", "title":"Home", "id":"home"}, {"page":"research", "title":"Research", "id":"research"}, {"title":"drp1"}]
+dropdowns = {"drp1":[{'title':'Analyses'}, {'title':"# of Delays", 'page':"byDelays"}, {'title':"# of Flights", 'page':"byFlights"}]}
 
 @app.route("/") #annotation tells the url that will make this function run
 def render_main():
-    return render_template("home.html", navItems=navItems, activePage="home")
+    return render_template("home.html", navItems=navItems, dropdowns=dropdowns, activePage="home")
 
 @app.route("/research") #annotation tells the url that will make this function run
 def render_research():
@@ -18,10 +19,10 @@ def render_research():
 
     airlinesNames = afx.getAirlines(airlines)
 
-    return render_template("research.html", airlines=airlinesNames, navItems=navItems, activePage="research")
+    return render_template("research.html", airlines=airlinesNames, navItems=navItems, dropdowns=dropdowns, activePage="research")
 
-@app.route("/analysis") #annotation tells the url that will make this function run
-def render_analysis():
+@app.route("/byDelays") #annotation tells the url that will make this function run
+def render_delays():
     with open('airlines.json') as inFile:
         airlines = json.load(inFile)
 
@@ -35,10 +36,10 @@ def render_analysis():
         maxAirport = afx.getAirlineWithMostDelaysAtAirport(afx.getDataForAirport(airlines, airportCode))
         delayData = afx.getDelayDataForAirport(afx.getDataForAirport(airlines, airportCode), airlines)
         matcher = afx.getAirlinesDict(airlines)
-        return render_template("analysis.html", opts=airports, navItems=navItems, activePage="analysis", current=airport, airport=airport, maxAirport=matcher[maxAirport], delayData=delayData)
+        return render_template("byDelays.html", opts=airports, navItems=navItems, dropdowns=dropdowns, activePage="byDelays", current=airport, airport=airport, maxAirport=matcher[maxAirport], delayData=delayData)
     except Exception as e:
         print(e)
-        return render_template("analysis.html", opts=airports, navItems=navItems, activePage="analysis", airport="!!!")
+        return render_template("byDelays.html", opts=airports, navItems=navItems, dropdowns=dropdowns, activePage="byDelays", airport="!!!")
 
 
 if __name__=="__main__":
